be433aa8ad5a0cf72dcb96ed7fcd675af27c82a4
Yunis-Mammadovm <yunismammadovv@gmail.com>
2024-02-22 23:57:59 +0400

    sacqulluq responsive vers 1 added

--- frontend/src/pages/User/SacQulluqDetail/index.jsx
+++ frontend/src/pages/User/SacQulluqDetail/index.jsx
@@ -4,7 +4,12 @@ import { useParams } from 'react-router-dom';
 import { useCart } from '../../../context/CartContext';
 import Swal from 'sweetalert2';
 import { getSacQulluqById } from '../../../api/request';
+import Accordion from '@mui/material/Accordion';
+import AccordionSummary from '@mui/material/AccordionSummary';
+import AccordionDetails from '@mui/material/AccordionDetails';
+import Typography from '@mui/material/Typography';
 import styles from "./index.module.css";
+
 
 
 const SacQulluqDetail = () => {
@@ -112,40 +117,64 @@ const SacQulluqDetail = () => {
     if (isMobile && !isExtraLarge) {
         return (
             <>
-                <div>
-                    {sacqulluq ? (
-                        <div className={styles.mobileParentDetailKeratin}>
-                            <div className={styles.mobileParentProductImg}>
-                                <img src={sacqulluq.productImgUrl} alt={sacqulluq.name} />
-                            </div>
-                            <div className={styles.mobileKeratinDetailInfo}>
-                                <div className={styles.mobileFirstDetail}>
-                                    <p>{sacqulluq.brand}</p>
-                                    <h2>{sacqulluq.name}</h2>
-                                    <p>{sacqulluq.description}</p>
-                                    <p className={styles.productDetails}>{sacqulluq.productDetails}</p>
+                {sacqulluq ? (
+                    <div>
+                        <div className={styles.flipCard}>
+                            <div className={styles.flipCardInner}>
+                                <div className={styles.flipCardFront}>
+                                    <img style={{ width: "100%", height: "100%" }} src={sacqulluq.productImgUrl} alt={sacqulluq.name} />
                                 </div>
-                                <div className={styles.mobileInputParent}>
-                                    <h3>Sayını seçin:</h3>
-                                    <div style={{ display: "flex", gap: "85px" }}>
-                                        <input
-                                            type="number"
-                                            id="sayi"
-                                            value={quantity}
-                                            min="1"
-                                            onChange={(e) => setQuantity(parseInt(e.target.value))}
-                                        />
-                                        <div className={styles.detailWhislistButton}>
-                                            <p onClick={addToLocalStorage}>Səbətə Əlavə Et</p>
-                                        </div>
+                                <div className={styles.flipCardBack}>
+                                    <div className={styles.mobileFirstDetail}>
+                                        <p style={{ color: "black" }}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusantium alias minima id quasi explicabo nihil. Vitae, consequatur alias! Iure, quos fugit ducimus eligendi ipsum quod quidem. Temporibus aliquam maxime repudiandae.</p>
                                     </div>
                                 </div>
                             </div>
+
                         </div>
-                    ) : (
-                        <p>Loading...</p>
-                    )}
-                </div>
+                        <div className={styles.mobileBrandName}>
+                            <p>{sacqulluq.brand}</p>
+                            <h4>{sacqulluq.name}</h4>
+                        </div>
+                        <div className={styles.mobileInputParent}>
+                            <div>
+                                <h3>Sayını seçin:</h3>
+                                <div style={{ display: "flex", gap: "85px" }}>
+                                    <input
+                                        type="number"
+                                        id="sayi"
+                                        value={quantity}
+                                        min="1"
+                                        onChange={(e) => setQuantity(parseInt(e.target.value))}
+                                    />
+                                </div>
+                            </div>
+                            <div className={styles.detailWhislistButton}>
+                                <p onClick={addToLocalStorage}>Səbətə Əlavə Et</p>
+                            </div>
+                            <div style={{
+                                display: "flex",
+                                justifyContent: "center",
+                                alignItems: "center",
+                                width: "90%",
+                                margin: "50px 0"
+                            }}>
+                                <Accordion defaultExpanded>
+                                    <AccordionSummary aria-controls="panel1a-content" id="panel1a-header">
+                                        <Typography>Məhsul Detalları</Typography>
+                                    </AccordionSummary>
+                                    <AccordionDetails>
+                                        <Typography>
+                                            {sacqulluq.productDetails}
+                                        </Typography>
+                                    </AccordionDetails>
+                                </Accordion>
+                            </div>
+                        </div>
+                    </div>
+                ) : (
+                    <p>Loading...</p>
+                )}
             </>
         );
     }
