24a5e1d448a00cf0e78370b6820f098b3cbff95f
Yunis-Mammadovm <yunismammadovv@gmail.com>
2024-02-23 01:48:58 +0400

    whatsapp floating added

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,14 +3,16 @@ import React, { useEffect } from 'react';
 import { RouterProvider, createBrowserRouter } from 'react-router-dom';
 import { CartProvider } from './context/CartContext';
 import { ROUTES } from './routes/ROUTES';
+import { FloatingWhatsApp } from 'react-floating-whatsapp'
+
 
 const theme = createTheme({
   palette: {
     primary: {
-      main: '#FFFFFF', // Ana renk
+      main: '#FFFFFF',
     },
     background: {
-      default: '#FFFFFF', // Arka plan rengi
+      default: '#FFFFFF',
     },
   },
 });
@@ -25,12 +27,20 @@ const App = () => {
 
   const routes = createBrowserRouter(ROUTES);
 
-
   return (
     <>
       <ThemeProvider theme={theme}>
         <CartProvider>
           <RouterProvider router={routes} />
+          <FloatingWhatsApp
+            phoneNumber="+994708878799"
+            accountName='Nanoplastica.az'
+            chatMessage='Salam. Sizə necə kömək edə bilərik?'
+            avatar='https://res.cloudinary.com/dsb3j1ozv/image/upload/v1704196009/NanoPlastica_1_qz64fz.jpg'
+            statusMessage='online'
+            notification
+            notificationSound	
+            />
         </CartProvider>
       </ThemeProvider>
     </>
---
+++ frontend/src/WhatsAppButton.js
@@ -0,0 +1,19 @@
+// import React from 'react';
+// import WhatsAppIcon from '@mui/icons-material/WhatsApp';
+// import styles from './index.module.css';
+
+// const WhatsAppButton = ({ phoneNumber }) => {
+//     const handleClick = () => {
+//         window.open(`https://wa.me/${phoneNumber}`, '_blank');
+//     };
+
+//     return (
+//         <div className={styles.whatsappButton}>
+//             <button onClick={handleClick}>
+//                 <WhatsAppIcon />
+//             </button>
+//         </div>
+//     );
+// };
+
+// export default WhatsAppButton;
