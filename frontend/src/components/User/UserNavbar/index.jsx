794e648c81216d32900a1081e00b6efbf26566ea
Yunis-Mammadovm <yunismammadovv@gmail.com>
2024-03-27 23:53:56 +0400

    add random logo

--- frontend/src/components/User/UserNavbar/index.jsx
+++ frontend/src/components/User/UserNavbar/index.jsx
@@ -148,9 +148,9 @@ const Navbar = () => {
               </div>
             </div>
             <div className={styles.navbar}>
-              {/* <div className={styles.logoContainer}>
-                <img src="https://res.cloudinary.com/dsb3j1ozv/image/upload/v1710185551/WhatsApp_Image_2024-03-11_at_23.30.17-removebg-preview_v7n93l.png" alt="" />
-              </div> */}
+              <div className={styles.logoContainer}>
+                <img src="https://yberausa.com/wp-content/uploads/2019/03/WHITE-NEW-YBERA-LOGO-1536x597.png" alt="" />
+              </div>
               <div className={styles.navbarLinks}>
                 <ul>
                   <li><Link to="/">{t('esas')}</Link></li>
@@ -581,9 +581,9 @@ const Navbar = () => {
                 </Drawer>
               </div>
             </div>
-            {/* <div className={styles.logoContainerMobile}>
-              <img src="https://res.cloudinary.com/dsb3j1ozv/image/upload/v1710185551/WhatsApp_Image_2024-03-11_at_23.30.17-removebg-preview_v7n93l.png" alt="" />
-            </div> */}
+            <div className={styles.logoContainer}>
+              <img src="https://yberausa.com/wp-content/uploads/2019/03/WHITE-NEW-YBERA-LOGO-1536x597.png" alt="" />
+            </div>
             <div>
               <Link to="/basket" onClick={handleDrawerClose}>
                 <LocalGroceryStoreIcon sx={{ fontSize: "28px", color: "white", backgroundColor: "transparent" }} />
