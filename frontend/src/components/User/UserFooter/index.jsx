1254d91eb06da8aeabca30f58b78adcc2625ea5b
Yunis-Mammadovm <yunismammadovv@gmail.com>
2024-04-02 23:23:31 +0400

    footer logo added

--- frontend/src/components/User/UserFooter/index.jsx
+++ frontend/src/components/User/UserFooter/index.jsx
@@ -40,7 +40,7 @@ const Footer = () => {
         <div className={styles.footerPartTwo}>
           <div style={{ display: "flex", justifyContent: "center", alignItems: "center", flexDirection: "column" }}>
             <div className={styles.logoFooter}>
-              <img src="https://res.cloudinary.com/dsb3j1ozv/image/upload/v1710185551/WhatsApp_Image_2024-03-11_at_23.30.17-removebg-preview_v7n93l.png" alt="" />
+              <img src="https://res.cloudinary.com/dsb3j1ozv/image/upload/v1712085676/WhatsApp_Image_2024-04-02_at_11.17.21_PM-removebg-preview_ayixmk.png" alt="" />
             </div>
             <div className={styles.sosialFooter}>
               <Link style={{ color: 'black' }} to={"https://www.instagram.com/ozonio.official.azerbaijan/?ref=oSa2ew7A&hl=am-et"}>
