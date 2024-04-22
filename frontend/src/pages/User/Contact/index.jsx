d3a365f5266d110b8badfa489d0e0ab7d70cffde
Yunis-Mammadovm <yunismammadovv@gmail.com>
2024-04-23 00:42:08 +0400

    contact page added

--- frontend/src/pages/User/Contact/index.jsx
+++ frontend/src/pages/User/Contact/index.jsx
@@ -1,71 +1,59 @@
-import React, { useState, useEffect } from 'react';
-import { useTranslation } from 'react-i18next';
+import React from 'react';
+import InstagramIcon from '@mui/icons-material/Instagram';
+import FacebookIcon from '@mui/icons-material/Facebook';
+import YouTubeIcon from '@mui/icons-material/YouTube';
+import EmailIcon from '@mui/icons-material/Email';
 import WhatsAppIcon from '@mui/icons-material/WhatsApp';
-import "./styles.css"
+import { Link } from 'react-router-dom';
+import { Grid } from '@mui/material';
+import styles from './index.module.css';
 
 const Contact = () => {
-  const [language, setLanguage] = useState('az');
-  const { t, i18n } = useTranslation();
-
-  useEffect(() => {
-    const handleLanguageChange = () => {
-      setLanguage(i18n.language);
-    };
-
-    i18n.on('languageChanged', handleLanguageChange);
-
-    return () => {
-      i18n.off('languageChanged', handleLanguageChange);
-    };
-  }, [i18n]);
-
   return (
-    <div id="section">
-      <section className="contact-wrap">
-        <form action="" className="contact-form">
-          <div className="row">
-            <div className="col-sm-6">
-              <div className="input-block">
-                <input type="text" className="form-control" placeholder={t("name")} />
-              </div>
-            </div>
-            <div className="col-sm-6">
-              <div className="input-block">
-                <input type="text" className="form-control" placeholder={t("surname")} />
-              </div>
-            </div>
-          </div>
-          <div className="row">
-            <div className="col-sm-12">
-              <div className="input-block">
-                <input type="email" className="form-control" placeholder={t("mail")} />
-              </div>
-            </div>
-          </div>
-          <div className="row">
-            <div className="col-sm-12">
-              <div className="input-block">
-                <input type="text" className="form-control" placeholder={t("subject")} />
-              </div>
-            </div>
-          </div>
-          <div className="row">
-            <div className="col-sm-12">
-              <div className="input-block textarea">
-                <textarea rows="3" className="form-control" placeholder={t("message")}></textarea>
-              </div>
-            </div>
-          </div>
-          <div className="row">
-            <div className="col-sm-12">
-              <button className="sendWpButton"><WhatsAppIcon /><p>
-                {t("send")}
-              </p>
-              </button>
-            </div>
-          </div>
-        </form>
-      </section>
+    <div className={styles.parentContact}>
+      <div className={styles.textContact}>
+        <h1 className={styles.headerContact}>Bizimlə Əlaqə</h1>
+        <p>Sosial Media vasitəsilə bizimlə əlaqə yarat</p>
+      </div>
+      <Grid container spacing={2} className={styles.iconContact}>
+        <Grid item xs={6} sm={3}>
+          <Link style={{ color: 'black', display: 'flex', flexDirection: 'column', justifyContent: 'center', gap: '10px' }} target="blank">
+            <FacebookIcon sx={{ margin: '0 auto', fontSize: '68px', border: '1px solid white', backgroundColor: 'rgb(245,245,245)', borderRadius: '50%', padding: '10px' }} />
+            <p style={{ textAlign: "center", fontSize: "14px" }}>
+              Ozonio Azerbijan
+            </p>
+          </Link>
+        </Grid>
+        <Grid item xs={6} sm={3}>
+          <Link style={{ color: 'black', display: 'flex', flexDirection: 'column', justifyContent: 'center', gap: '10px' }} target="blank" to={"https://www.instagram.com/ozonio.official.azerbaijan/?ref=oSa2ew7A&hl=am-et"}>
+            <InstagramIcon sx={{ margin: '0 auto', fontSize: '68px', border: '1px solid white', backgroundColor: 'rgb(245,245,245)', borderRadius: '50%', padding: '10px' }}  />
+            <p style={{ textAlign: "center", fontSize: "14px" }}>
+              ozonio.official.azerbaijan
+            </p>
+          </Link>
+        </Grid>
+        <Grid item xs={6} sm={3}>
+          <Link style={{ color: 'black', display: 'flex', flexDirection: 'column', justifyContent: 'center', gap: '10px' }} target="blank" to="https://wa.me/994708878799">
+            <WhatsAppIcon sx={{ margin: '0 auto', fontSize: '68px', border: '1px solid white', backgroundColor: 'rgb(245,245,245)', borderRadius: '50%', padding: '10px' }} />
+            <p style={{ textAlign: "center", fontSize: "14px" }}>
+              0708878799
+            </p>
+          </Link>
+        </Grid>
+        <Grid item xs={6} sm={3}>
+          <Link style={{ color: 'black', display: 'flex', flexDirection: 'column', justifyContent: 'center', gap: '10px' }} target="blank" to={"https://www.youtube.com/@nanoplastica_az"}>
+            <YouTubeIcon sx={{ margin: '0 auto', fontSize: '68px', border: '1px solid white', backgroundColor: 'rgb(245,245,245)', borderRadius: '50%', padding: '10px' }} />
+            <p style={{ textAlign: "center", fontSize: "14px" }}>
+              Ozonio Azerbijan
+            </p>
+          </Link>
+        </Grid>
+        <Grid item xs={6} sm={3}>
+          {/* <Link style={{ color: 'black', display: 'flex', flexDirection: 'column', justifyContent: 'center', gap: '10px' }} target="blank">
+            <EmailIcon sx={{ fontSize: '68px', border: '1px solid white', backgroundColor: 'rgb(245,245,245)', borderRadius: '50%', padding: '10px' }} />
+          </Link> */}
+        </Grid>
+      </Grid>
     </div>
   );
 };
